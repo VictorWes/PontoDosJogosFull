1a0514245f748ac8906af2ddb821010db1c01f50
Victor Wesley <victor.infowb@gmail.com>
2025-11-05 18:55:14 -0300

    chore: Estrutura inicial projeto

---
+++ src/app/views/carrinho-view/carrinho-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho-view',
+  imports: [],
+  templateUrl: './carrinho-view.component.html',
+  styleUrl: './carrinho-view.component.css'
+})
+export class CarrinhoViewComponent {
+
+}
---
+++ src/app/views/catalogo-view/catalogo-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-view',
+  imports: [],
+  templateUrl: './catalogo-view.component.html',
+  styleUrl: './catalogo-view.component.css'
+})
+export class CatalogoViewComponent {
+
+}
