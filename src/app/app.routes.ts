20fbff4d2d43634340bed4b552aa7f4bb1973159
Victor Wesley <victor.infowb@gmail.com>
2025-11-17 18:55:23 -0300

    feat:Adiciona estrutura home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginViewComponent } from './views/login-view/login-view.component';
-import { CatalogoViewComponent } from './views/catalogo-view/catalogo-view.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+
 export const routes: Routes = [
   { path: '', component: LoginViewComponent },
+  { path: 'home', component: HomeViewComponent },
 ];
