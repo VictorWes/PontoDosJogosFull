cd4fb837e6b01a4176d43142de95622297ad50f0
Victor Wesley <victor.infowb@gmail.com>
2025-11-17 19:06:10 -0300

    feat: Adiciona footer e header em home-view

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-view',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
